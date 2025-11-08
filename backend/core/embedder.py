41005276cb0f944499d5dd953c9e0d73c0ff3188
prakharstech <prakhar.srivastava0509@gmail.com>
2025-11-09 03:15:33 +0530

    Added regex

--- backend/core/embedder.py
+++ backend/core/embedder.py
@@ -1,10 +1,12 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-import logging
+import logging, os
 
 def build_vector_store(chunks):
     """Build an in-memory FAISS vector store from document chunks."""
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    #embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    embeddings = OpenAIEmbeddings(api_key=os.getenv("OPENAI_API_KEY"))
     vector_store = FAISS.from_documents(chunks, embeddings)
     logging.info("Vector store created successfully.")
     return vector_store
